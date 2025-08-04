ddb61c9b19d09483874995366f0aa04f5cef2fb3
constertine <shivi98k@gmail.com>
2025-08-04 18:53:06 +0530

    Fix: updated DB connection logic in index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:["http://localhost:3000","https://learnova-7f4v.onrender.com"],
 		credentials:true,
 	})
 )
